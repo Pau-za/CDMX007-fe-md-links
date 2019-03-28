818014f37d4890fc6c6e36764dbc2f2e3b73702d
Pau-za <molo_pau@hotmail.com>
2019-03-28 13:03:37 -0600

    program.js obtiene la ruta del archivo md que ingresa usuario, y lee el documento completo. Ahora falta hacer TEST!!! Y después que solo liste los links que encuentre :O

--- src/program.js
+++ src/program.js
@@ -1,36 +1,20 @@
 // este programa busca un programa que dado un directorio imprime una lista de archivos filtrados por la extensión
 const fs = require('fs');
-const pathE = require('path');
+// const path = require('path');
 
 // cuando el usuario no introduce la ruta de la carpeta que quiere buscar
 module.exports = program = () => {
-  console.log(process.argv.length);
   if (process.argv.length <= 2) {
     console.log("Usage: " + __filename + " path/to/directory");
     process.exit(-1);
   }
 
-  const path = process.argv[2]; //define la ruta de la carpeta que proporciona el usuario para buscar
-  fs.readdir(path, (err, items) => {
-    console.log(path);
+  const path = process.argv[2];
+  // fs.readFile lee un documento de forma asícrona de acuerdo a la ruta, url que le pases :D
+  fs.readFile(`${path}`, 'utf8', (err, data) => {
+    //utf8 le pide que retorne el contenido del documento (data) como un string 
     if (err) throw err;
-    // console.log(items);
-
-    for (var i = 0; i < items.length; i++) {
-      console.log(items[i]);
-      const ext = pathE.extname(items[i]);
-      console.log(ext);
-
-      if (ext === '.md') {
-        console.log(items[i]);
-        // fs.readFile lee un documento de forma asícrona de acuerdo a la ruta, url que le pases :D
-        fs.readFile(`${items[i]}`, 'utf8', (err, data) => {
-          //utf8 le pide que retorne el contenido del documento (data) como un string 
-          if (err) throw err;
-          console.log(data);
-        });
-      }
-    }
+    console.log(data);
   });
 }
 
