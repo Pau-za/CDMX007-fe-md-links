e9f55cca08b9aa7360196f7df3bd98920425f686
Pau-za <molo_pau@hotmail.com>
2019-03-28 10:42:39 -0600

    Función suma en index.js que recorre el arreglo que crea process.argv, para hacer una sumatoria de cada número que se encuentre a partir de la tercera posición del arreglo :D

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-module.exports = () => {
-  // ...
-};
+module.exports =
+  sum = () => {
+    let add = 0;
+    for (let i = 2; i < process.argv.length; i++) {
+      add += Number(process.argv[i]);
+    }
+    return add
+  }
+
+console.log(sum());
+
+// console.log(Number(process.argv[2, 3]));
+
+
+// console.log(sum(4, 5))
