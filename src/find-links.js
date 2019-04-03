6f86929c76f0b71d42ce65600ae898f7eb6e7cd5
Pau-za <molo_pau@hotmail.com>
2019-04-02 20:38:20 -0600

    guarda la información de los links (nombre y url) en un solo arreglo :D

--- src/find-links.js
+++ src/find-links.js
@@ -3,35 +3,41 @@ const path = require('path');
 const md = require('markdown-it')();
 const mila = require('markdown-it-link-attributes');
 const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+const {
+  JSDOM
+} = jsdom;
 const validate = require('./get-link');
 
 module.exports = findLinks = () => {
-    if (process.argv.length <= 2) {
-        console.log("Necesitas ingresar la ruta de un archivo con extensión md");
-        process.exit(-1);
-      }
+  if (process.argv.length <= 2) {
+    console.log("Necesitas ingresar la ruta de un archivo con extensión md");
+    process.exit(-1);
+  }
 
-      const userPath = path.resolve(process.argv[2]);
-      fs.readFile(`${userPath}`, 'utf8', (err, data) => {
-          if(err) throw err;
-        
-         const renderedReadme =  md.render(data); // convierte el readme a html
-    
-        totalLinks = [];
-          const obj = new JSDOM(renderedReadme).window.document.getElementsByTagName("a");
-        for (const key in obj) {
-            if (obj.hasOwnProperty(key)) {
-                totalLinks.push(obj[key].getAttribute("href"));
-                const link = obj[key].getAttribute("href");
-                const linkName = obj[key].textContent;
-                // console.log('link ' + key + ' ' + linkName + ': ' + link);
-            }
-        }
-        validate(totalLinks);
-        // console.log(totalLinks);
-     
-    })
+  const userPath = path.resolve(process.argv[2]);
+  fs.readFile(`${userPath}`, 'utf8', (err, data) => {
+    if (err) throw err;
+
+    const renderedReadme = md.render(data); // convierte el readme a html
+
+    let totalLinks = [];
+    let totalNamesLinks = [];
+    let linksInfo = [];
+    const obj = new JSDOM(renderedReadme).window.document.getElementsByTagName("a");
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        totalLinks.push(obj[key].getAttribute("href"));
+        const link = obj[key].getAttribute("href");
+        const linkName = obj[key].textContent;
+        totalNamesLinks.push(obj[key].textContent);
+        // console.log('link ' + key + ' ' + linkName + ': ' + link);
+        linksInfo.push(`${'link a: ' + linkName + ', url: ' + link}`)
+      }
+    }
+    // validate(totalLinks);
+    console.log(linksInfo);
+
+  })
 }
 
 console.log(findLinks());
--- src/get-link.js
+++ src/get-link.js
@@ -3,64 +3,72 @@ const http = require('http');
 
 module.exports =
   validate = (links) => {
+    let activeLinks = [];
+    let brokenLinks = [];
     links.forEach(element => {
       if (element.includes("https")) {
         https.get(`${element}`, (res) => {
-            if(res.statusCode === 404){
-                console.log(element + ' página no encontrada')
-            } else {
-                console.log(element + ' página activa')
-            }
-        //   console.log('statusCode:', res.statusCode);
-        //   console.log('headers:', res.headers);
+          if (res.statusCode === 404) {
+            brokenLinks.push(element);
+            // console.log(brokenLinks);
+            return
+          } else {
+            activeLinks.push(element);
+            // console.log(activeLinks);
+            return
+          }
+          //   console.log('statusCode:', res.statusCode);
+          //   console.log('headers:', res.headers);
 
-        //   res.on('data', (d) => {
-        //     process.stdout.write(d);
-        //   });
+          //   res.on('data', (d) => {
+          //     process.stdout.write(d);
+          //   });
 
         }).on('error', (e) => {
           console.error(e);
         })
         // console.log(element);
         // return
-    //   } else {
-    //     http.get(`${element}`, (res) => {
-    //       const {
-    //         statusCode
-    //       } = res;
-    //       const contentType = res.headers['content-type'];
+        //   } else {
+        //     http.get(`${element}`, (res) => {
+        //       const {
+        //         statusCode
+        //       } = res;
+        //       const contentType = res.headers['content-type'];
 
-    //       let error;
-    //       if (statusCode !== 200) {
-    //         error = new Error('Request Failed.\n' +
-    //           `Status Code: ${statusCode}`);
-    //       } else if (!/^application\/json/.test(contentType)) {
-    //         error = new Error('Invalid content-type.\n' +
-    //           `Expected application/json but received ${contentType}`);
-    //       }
-    //       if (error) {
-    //         console.error(error.message);
-    //         // Consume response data to free up memory
-    //         res.resume();
-    //         return;
-    //       }
+        //       let error;
+        //       if (statusCode !== 200) {
+        //         error = new Error('Request Failed.\n' +
+        //           `Status Code: ${statusCode}`);
+        //       } else if (!/^application\/json/.test(contentType)) {
+        //         error = new Error('Invalid content-type.\n' +
+        //           `Expected application/json but received ${contentType}`);
+        //       }
+        //       if (error) {
+        //         console.error(error.message);
+        //         // Consume response data to free up memory
+        //         res.resume();
+        //         return;
+        //       }
 
-    //       res.setEncoding('utf8');
-    //       let rawData = '';
-    //       res.on('data', (chunk) => {
-    //         rawData += chunk;
-    //       });
-    //       res.on('end', () => {
-    //         try {
-    //           const parsedData = JSON.parse(rawData);
-    //           console.log(parsedData);
-    //         } catch (e) {
-    //           console.error(e.message);
-    //         }
-    //       });
-    //     }).on('error', (e) => {
-    //       console.error(`Got error: ${e.message}`);
-    //     });
+        //       res.setEncoding('utf8');
+        //       let rawData = '';
+        //       res.on('data', (chunk) => {
+        //         rawData += chunk;
+        //       });
+        //       res.on('end', () => {
+        //         try {
+        //           const parsedData = JSON.parse(rawData);
+        //           console.log(parsedData);
+        //         } catch (e) {
+        //           console.error(e.message);
+        //         }
+        //       });
+        //     }).on('error', (e) => {
+        //       console.error(`Got error: ${e.message}`);
+        //     });
       }
+      console.log(brokenLinks + ' página no encontrada')
+      console.log(activeLinks + ' página activa')
     });
   };
