477230bcd8397367fcd506201e0378d90997791d
Pau-za <molo_pau@hotmail.com>
2019-04-04 20:54:38 -0600

    intenta hacer test

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,20 @@
-const mdLinks = require('../');
+const mdLinks = require('../mdLinks');
+const validate = require('../src/validate');
+const stats = require('../src/stats');
+const validateStats = require('../src/validate-stats');
+
+const mockedPath = [
+  ['something'],
+  ['another-thing'],
+  ['this/is/a/path' ['README.md' ['[a-link](https//:this.is.a/link/)']]]
+];
 
 
 describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
-  });
-
-});
+  it('should be a function', () => {
+    expect(typeof window.mdLinks).toBe('function');
+  })
+  it('should return a two dimensions array for any path user enters, and no options'), () => {
+    expect(window.mdLinks(mockedPath)).toEqual('Link:  https//:this.is.a/link/,  to: a-link');
+    }
+})
